56963b915429af2b67e835887183caf0d16727d7
ShaliniS FEBootCamper <118174874+ShaliniSunkuru@users.noreply.github.com>
2024-03-11 13:34:12 +0000

    moved images to public folder so they render when deployed

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,7 +3,7 @@ import './pages.css';
 function Resume(){
     return(
         <div className='page'>
-                <object className="pdf" width="100%" height="600" data="/src/images/ShaliniSunkuru.pdf" type="application/pdf">   </object>               
+                <object className="pdf" width="100%" height="600" data="/images/ShaliniSunkuru.pdf" type="application/pdf">Shalini's Resume</object>               
         </div>
     )
 }
