36fed2cebe10507aa3338f96a9c846052f37f83b
ShaliniS FEBootCamper <118174874+ShaliniSunkuru@users.noreply.github.com>
2024-03-12 16:19:24 +0000

    added onchange and onsubmit events to contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,17 +6,71 @@ import Footer from '../components/Footer';
 
 function Contact(){
 
-    const [ visitorName, setVisitorName ] = useState('');
+    const [ formData, setFormData ] = useState({
+        visitorName: '',
+        visitorEmail: '',
+        message: ''
+    });
     
+    const handleInputChange = (event) => {
+        //setting variables name and value to event.target.name and event.target.value
+        //i.e, getting the name and value of input which triggered change
+        const {name, value} = event.target;
+
+        //set formData to current values
+        setFormData({
+            // copy of object - formData
+            ...formData,
+            [name]: value,
+        });
+    }
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(`${formData.visitorName} \n ${formData.visitorEmail} \n ${formData.message}`)
+    }
     return(
         <div>
             <form className='contactForm'>
             <h3>Contact Me</h3>
             <div className='mx-5'>
-                <input type="text" className='form-control my-3' id='visitornName' placeholder='Your name please' required/>                              
-                <input type="email" className="form-control mb-3" id="visitorEmail" placeholder="Enter your email id" required/>
-                <textarea class="form-control mb-3" id="exampleFormControlTextarea1" rows="3" placeholder='Type your message here' required></textarea>
-                <button type="submit" class="btn btn-secondary mb-3" id='submitBtn' >Send Message</button>
+                <input 
+                    type="text" 
+                    className='form-control my-3' 
+                    id='visitornName' 
+                    placeholder='Your name please' 
+                    required
+                    name='visitorName'
+                    value={formData.visitorName}
+                    onChange={handleInputChange}
+                />                              
+                <input 
+                    type="email" 
+                    className="form-control mb-3" 
+                    id="visitorEmail" 
+                    placeholder="Enter your email id" 
+                    required
+                    name='visitorEmail'
+                    value={formData.visitorEmail}
+                    onChange={handleInputChange}
+                />
+                <textarea 
+                    className="form-control mb-3" 
+                    id="message" 
+                    rows="3" 
+                    placeholder='Type your message here' 
+                    required
+                    name='message'
+                    value={formData.message}
+                    onChange={handleInputChange}
+                ></textarea>
+                <button 
+                    type="submit" 
+                    className="btn btn-secondary mb-3" 
+                    id='submitBtn'
+                    onClick={handleFormSubmit}
+                >
+                    Send Message
+                </button>
             </div>         
         </form>
 
